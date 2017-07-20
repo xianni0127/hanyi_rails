3cd276884b2a3f3297d1a3ab707b8ee446934402
xianni0127 <xianni0127@sina.com>
2017-07-20 22:49:13 +0800

    let unit test pass

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,8 @@ module FisrtRails
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
+    config.action_mailer.default_url_options = {host:'http://www.sina.com'}
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
