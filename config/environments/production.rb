a99e46521468dbd2b983ef2ceadd456cfc59eba5
xianni0127 <xianni0127@sina.com>
2017-07-23 13:54:07 +0800

    add db_test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Rails.application.configure do
    domain:               "gmail.com",
    user_name:            'jennyxian0127@gmail.com',
    password:             'Wych0604*',
-   authentication:       'plain',
+   authentication:       nil,
    enable_starttls_auto: true
 }
   # Use a different cache store in production.
