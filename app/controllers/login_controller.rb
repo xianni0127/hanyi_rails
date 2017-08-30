544720300398b74ac60573efbac41e9a8b67799f
xianni0127 <xianni0127@sina.com>
2017-08-30 17:52:28 +0800

    add login contrlloer

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,2 +1,19 @@
 class LoginController < ApplicationController
+  def login
+    unless request.get?
+      childname=params[:child][:name]
+      password=params[:child][:password]
+      @hint = "ok"
+      @child = Child.find_by_name(childname)
+      if @child.class == NilClass
+        @hint = 'Unregisted Children!'
+      elseif @child.password == params[:child][:password]
+        @hint = "Welcome back,"
+        redirect_to :controller=>'login', :action=>'index'
+      else
+        @hint="Login failed, please check up your info"
+  end
+
+  def index
+  end
 end
